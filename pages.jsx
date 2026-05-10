dd54504f8fb838e6a0f19987ec98ff5c1780bc7b
Clinton Hall <clintonbazzle@gmail.com>
2026-05-10 12:18:10 -0400

    about: swap bottom photos and fix aspect ratio

--- pages.jsx
+++ pages.jsx
@@ -807,8 +807,8 @@ function AboutPage({ setPage }) {
           </div>
         </div>
         <div className="about-photos-below">
-          <div className="cp-slot"><img src="images/about-img_6519-2.jpg" alt="" style={{width:"100%",height:"100%",objectFit:"cover"}}/></div>
-          <div className="cp-slot"><img src="images/about-img_1989-3.jpg" alt="" style={{width:"100%",height:"100%",objectFit:"cover"}}/></div>
+          <div className="cp-slot"><img src="images/about-img_1625-3.jpg" alt="" style={{width:"100%",height:"100%",objectFit:"cover"}}/></div>
+          <div className="cp-slot"><img src="images/about-img_1326.jpg" alt="" style={{width:"100%",height:"100%",objectFit:"cover"}}/></div>
         </div>
       </section>
 
